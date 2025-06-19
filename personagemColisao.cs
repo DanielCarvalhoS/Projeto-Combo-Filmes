c404f30bbf01b8e5e5cee85dcf4b92872f6c014e
ChristianVarandas <162368958+ChristianVarandas@users.noreply.github.com>
2025-06-19 18:35:37 -0300

    Add files via upload

---
+++ personagemColisao.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Jogador
+{
+    public int life { get; private set; }
+
+    public Jogador(int vidaInicio)
+    {
+        life = vidaInicio;
+    }
+
+    public void PerdeVida()
+    {
+        life -= 1;
+    }
+
+    public bool Derrotado()
+    {
+        return life <= 0;
+    }
+}
+
+public class personagemColisao : MonoBehaviour
+{
+    private Jogador jogador;
+
+    void Start()
+    {
+        jogador = new Jogador(3); //vida inicial
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "obstaculo":
+                jogador.PerdeVida();
+                Debug.Log("Vidas restantes:" + jogador.life);
+
+                if (jogador.Derrotado())
+                {
+                    SceneManager.LoadScene("Fase 3");
+                }
+                break;
+
+            case "tesouro":
+                Debug.Log("objetivo concluído!");
+                break;
+        }
+    }
+
+}
